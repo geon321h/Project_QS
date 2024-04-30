eee4a2a82634e34db0a0bab3bf640ec39486511d
Geon <geon321h@gmail.com>
2024-04-30 12:47:00 +0900

    컨텐츠와 퀴즈 DTO 생성

--- Project_QS_NamGeon/src/main/java/QS/CONTENT/Content_List_DTO.java
+++ Project_QS_NamGeon/src/main/java/QS/CONTENT/Content_List_DTO.java
@@ -1,5 +1,73 @@
 package QS.CONTENT;
 
-public class Content_List_Bean {
+import java.util.ArrayList;
 
+public class Content_List_DTO {
+
+	private int content_key ;
+	private String title;
+	private String explanation ;
+	private int create_user ;
+	private String thumbnail ;
+	private int content_count  ;
+	private String content_public ;
+	private String content_type;
+	private String create_day  ;
+
+	
+	public int getContent_key() {
+		return content_key;
+	}
+	public void setContent_key(int content_key) {
+		this.content_key = content_key;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getExplanation() {
+		return explanation;
+	}
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
+	public int getCreate_user() {
+		return create_user;
+	}
+	public void setCreate_user(int create_user) {
+		this.create_user = create_user;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public int getContent_count() {
+		return content_count;
+	}
+	public void setContent_count(int content_count) {
+		this.content_count = content_count;
+	}
+	public String getContent_public() {
+		return content_public;
+	}
+	public void setContent_public(String content_public) {
+		this.content_public = content_public;
+	}
+	public String getContent_type() {
+		return content_type;
+	}
+	public void setContent_type(String content_type) {
+		this.content_type = content_type;
+	}
+	public String getCreate_day() {
+		return create_day;
+	}
+	public void setCreate_day(String create_day) {
+		this.create_day = create_day;
+	}
+	
 }
---
+++ Project_QS_NamGeon/src/main/java/QS/CONTENT/Quiz_List_DTO.java
@@ -0,0 +1,49 @@
+package QS.CONTENT;
+
+public class Quiz_List_DTO {
+
+	private int quiz_key  ;
+	private int content_key ;
+	private String question;
+	private String image;
+	private String example;
+	private String answer ;
+	
+	public int getQuiz_key() {
+		return quiz_key;
+	}
+	public void setQuiz_key(int quiz_key) {
+		this.quiz_key = quiz_key;
+	}
+	public int getContent_key() {
+		return content_key;
+	}
+	public void setContent_key(int content_key) {
+		this.content_key = content_key;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getExample() {
+		return example;
+	}
+	public void setExample(String example) {
+		this.example = example;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+}
